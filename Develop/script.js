1dcd85a2a6768fda4f134f7f2a9f2b0d03f8e145
christian <christiancarvajal@Christians-MacBook-Air.local>
2022-04-09 17:53:04 -0400

    varablies and window prompts added

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 // Assignment code here
-
-
+var upper;
+var lower;
+var num;
+var special;
+var passwordLength;
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +18,51 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+generateBtn.onclick = function()
+{
+  passwordLength = window.prompt("Enter a number between 8 and 128 for your password length");
+if(passwordLength < 8 || passwordLength > 128)
+{
+  passwordLength = window.prompt("Enter a number between 8 and 128 for your password length");
+}
+
+if (confirm("Would you like to use uppercase characters")){
+  upper = true;
+  console.log(upper);
+}
+else{
+  upper = false;
+  console.log(upper);
+}
+if (confirm("Would you like to to use lowercase characters")){
+  lower = true;
+  console.log(lower);
+}
+else {
+  lower = false;
+  console.log(lower);
+}
+if(confirm("Would you like to use numbers?")){
+  num = true;
+  console.log(num);
+}
+else{
+  num = false;
+  console.log(num);
+}
+if(confirm("Would you like to use specail characters?")){
+  special = true;
+  console.log(special);
+}
+else{
+  special = false;
+  console.log(special);
+}
+console.log(passwordLength);
+if (upper == false && lower == false && num == false && special == false){
+  window.alert("choose at least one of these options");
+  generateBtn.onclick();
+}
+}
+
