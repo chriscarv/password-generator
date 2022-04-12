a88f9ac34726c52a9ba0bc8aff7b7d79fd349bea
christian <christiancarvajal@Christians-MacBook-Air.local>
2022-04-12 00:55:28 -0400

    generate password function

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,59 @@
 // Assignment code here
-var upper;
-var lower;
-var num;
-var special;
-var passwordLength;
+
+const upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerAlpha = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "012345678";
+const symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// generate password function
+function generatePassword(){
+var passwordvalue="";
+var placeholder="";
+
+var length = prompt("Choose a password length between 8 and 128 characters");
+if(length < 8 || length > 128){
+  alert("password must between 8 and 128 characters. try again")
+  return;
+}
+
+var upper = confirm("would you like to include uppercase characters?");
+if(upper){
+  placeholder += upperAlpha;
+  console.log(upper);
+}
+
+var lower = confirm("would you like to include lowercase characters?");
+if(lower){
+  placeholder += lowerAlpha;
+  console.log(lower);
+}
+
+var num = confirm("would you like to inlcude numbers?");
+if(num){
+  placeholder += numbers;
+  console.log(num);
+}
+
+var special = confirm("would you like to include specail characters?");
+if(special){
+  placeholder += symbols;
+  console.log(num);
+}
+if(upper == false && lower == false && num == false && special == false)
+{
+  alert("sorry. you must choose at least one of the following choices to continue");
+  return;
+}
+for(var i=0; i < length; i++){
+  var ranndomNumber = Math.floor(Math.random() * placeholder.length);
+  passwordvalue += placeholder.substring(ranndomNumber, ranndomNumber +1);
+}
+console.log(passwordvalue);
+return passwordvalue;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -16,53 +64,4 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-generateBtn.onclick = function()
-{
-  passwordLength = window.prompt("Enter a number between 8 and 128 for your password length");
-if(passwordLength < 8 || passwordLength > 128)
-{
-  passwordLength = window.prompt("Enter a number between 8 and 128 for your password length");
-}
-
-if (confirm("Would you like to use uppercase characters")){
-  upper = true;
-  console.log(upper);
-}
-else{
-  upper = false;
-  console.log(upper);
-}
-if (confirm("Would you like to to use lowercase characters")){
-  lower = true;
-  console.log(lower);
-}
-else {
-  lower = false;
-  console.log(lower);
-}
-if(confirm("Would you like to use numbers?")){
-  num = true;
-  console.log(num);
-}
-else{
-  num = false;
-  console.log(num);
-}
-if(confirm("Would you like to use specail characters?")){
-  special = true;
-  console.log(special);
-}
-else{
-  special = false;
-  console.log(special);
-}
-console.log(passwordLength);
-if (upper == false && lower == false && num == false && special == false){
-  window.alert("choose at least one of these options");
-  generateBtn.onclick();
-}
-}
-
